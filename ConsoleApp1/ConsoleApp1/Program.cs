28cb05d32ea3b54031bbee49f1609267fd4aa0c0
romanpapayevych@gmail.com <romanpapayevych@gmail.com>
2026-04-05 20:44:59 +0300

    added csproj file to fix problem with action

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, DevOps World!");
+            Console.WriteLine($"Current Date: {DateTime.Now}");
+        }
+        public static int Add(int a, int b) => a + b;
+    }
+}
